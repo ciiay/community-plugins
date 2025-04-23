f2ef9d58bc1e99f24dad9d09684c67fb6b775810
Yi Cai <yicai@redhat.com>
2025-04-23 15:07:50 -0400

    addressed review comments and added a common plugin
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- workspaces/servicenow/packages/app/src/components/search/SearchPage.tsx
+++ workspaces/servicenow/packages/app/src/components/search/SearchPage.tsx
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { default as React } from 'react';
 import { makeStyles, Theme, Grid, Paper } from '@material-ui/core';
 
 import { CatalogSearchResultListItem } from '@backstage/plugin-catalog';
