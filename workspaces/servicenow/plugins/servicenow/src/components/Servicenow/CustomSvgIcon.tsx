1f9d39bbfb854b4309d615acb45ce1dbec03039f
Yi Cai <yicai@redhat.com>
2025-04-29 00:32:22 -0400

    feat(servicenow): added incidents table
    Signed-off-by: Yi Cai <yicai@redhat.com>

--- workspaces/servicenow/plugins/servicenow/src/components/Servicenow/CustomSvgIcon.tsx
+++ workspaces/servicenow/plugins/servicenow/src/components/Servicenow/CustomSvgIcon.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Backstage Authors
+ * Copyright 2024 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { default as React } from 'react';
-import '@backstage/cli/asset-types';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import '@backstage/canon/css/styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+import { default as React } from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+
+export const CustomSvgIcon = ({
+  path,
+  viewBox = '0 0 24 24',
+  ...props
+}: any) => {
+  return (
+    <SvgIcon viewBox={viewBox} {...props}>
+      {path}
+    </SvgIcon>
+  );
+};
