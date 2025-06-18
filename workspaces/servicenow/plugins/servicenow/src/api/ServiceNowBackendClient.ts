668bc6d6d1113a71ad1669869b4e16be78189a84
Oleksandr Andriienko <oandriie@redhat.com>
2025-06-18 23:55:44 +0300

    feat(servicenow): implement ServiceNowBackendClient
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

---
+++ workspaces/servicenow/plugins/servicenow/src/api/ServiceNowBackendClient.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Incident } from '@backstage-community/plugin-servicenow-common';
+import {
+  createApiRef,
+  DiscoveryApi,
+  FetchApi,
+  IdentityApi,
+} from '@backstage/core-plugin-api';
+
+export interface ServiceNowBackendAPI {
+  getIncidents(): Promise<Incident[]>;
+}
+
+export const serviceNowApiRef = createApiRef<ServiceNowBackendAPI>({
+  id: 'plugin.servicenow.service',
+});
+
+export class ServiceNowBackendClient implements ServiceNowBackendAPI {
+  constructor(
+    private readonly discoveryApi: DiscoveryApi,
+    private readonly fetchApi: FetchApi,
+    private readonly identityApi: IdentityApi,
+  ) {}
+
+  private async fetchFromServiceNow<T>(path: string): Promise<T> {
+    const proxyBase = await this.discoveryApi.getBaseUrl('servicenow');
+    const url = `${proxyBase}${path}`;
+
+    const { token } = await this.identityApi.getCredentials();
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await this.fetchApi.fetch(url, { headers });
+    if (!response.ok) {
+      throw new Error(`ServiceNow API request failed: ${response.status}`);
+    }
+    return (await response.json()) as T;
+  }
+
+  async getIncidents(): Promise<Incident[]> {
+    return this.fetchFromServiceNow<Incident[]>('/incidents');
+  }
+}
