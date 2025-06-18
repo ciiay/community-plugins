7714fe2e6d0ed71a136c98d6e6175d3dae2cb106
Oleksandr Andriienko <oandriie@redhat.com>
2025-06-18 18:40:37 +0300

    feat(servicenow): move common type to the types.ts
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- workspaces/servicenow/plugins/servicenow-common/src/types.ts
+++ workspaces/servicenow/plugins/servicenow-common/src/types.ts
@@ -14,19 +14,11 @@
  * limitations under the License.
  */
 
-/**
- * Common functionalities for the servicenow plugin.
- *
- * @packageDocumentation
- */
-
-/**
- * Incident common type - represents a ServiceNow incident, but with limited set of the fields.
- * So, it is not meant to be a complete representation of a ServiceNow incident.
- * This is used to ensure that the incident data is consistent across different parts of the plugin.
- * It should be used in the frontend to display incident data in a consistent way.
- * It is used in the backend to fetch incident data from ServiceNow.
- * @public
- */
-
-export * from './types';
+export type Incident = {
+  number: string;
+  short_description: string;
+  description: string;
+  sys_created_on: string;
+  priority: string;
+  incident_state: string;
+};
